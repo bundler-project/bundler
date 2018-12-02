07c5aa5cf4f943fdcd3721f0a1709fd7d20fa041
Akshay Narayan <akshayn@mit.edu>
2018-12-02 01:01:33 +0000

    deps: statically link libccp

--- build.rs
+++ build.rs
@@ -10,30 +10,30 @@ fn main() {
     println!("cargo:rustc-link-lib=nl-3");
 
     let nl_bindings = bindgen::Builder::default()
-            .header("nl-route.h")
-            .clang_arg("-I/usr/include/libnl3")
-            .whitelist_function("nl_socket_alloc")
-            .whitelist_function("nl_connect")
-            .whitelist_function("rtnl_link_alloc_cache")
-            .whitelist_function("rtnl_link_get_by_name")
-            .whitelist_function("rtnl_link_get_ifindex")
-            .whitelist_function("rtnl_qdisc_alloc_cache")
-            .whitelist_function("rtnl_qdisc_alloc")
-            .whitelist_function("rtnl_qdisc_get")
-            .whitelist_function("rtnl_tc_get_stat")
-            .whitelist_function("rtnl_qdisc_put")
-            .whitelist_function("rtnl_qdisc_add")
-            .whitelist_function("TC_CAST")
-            .whitelist_function("TC_HANDLE")
-            .whitelist_function("rtnl_qdisc_tbf_set_rate")
-            .whitelist_function("nl_object_get_type")
-            .whitelist_function("nl_cache_get_first")
-            .whitelist_function("nl_cache_nitems")
-            .whitelist_var("NETLINK_ROUTE")
-            .whitelist_var("AF_UNSPEC")
-            .whitelist_var("NLM_F_REPLACE")
-            .generate()
-            .expect("unable to generate netlink-route bindings");
+        .header("nl-route.h")
+        .clang_arg("-I/usr/include/libnl3")
+        .whitelist_function("nl_socket_alloc")
+        .whitelist_function("nl_connect")
+        .whitelist_function("rtnl_link_alloc_cache")
+        .whitelist_function("rtnl_link_get_by_name")
+        .whitelist_function("rtnl_link_get_ifindex")
+        .whitelist_function("rtnl_qdisc_alloc_cache")
+        .whitelist_function("rtnl_qdisc_alloc")
+        .whitelist_function("rtnl_qdisc_get")
+        .whitelist_function("rtnl_tc_get_stat")
+        .whitelist_function("rtnl_qdisc_put")
+        .whitelist_function("rtnl_qdisc_add")
+        .whitelist_function("TC_CAST")
+        .whitelist_function("TC_HANDLE")
+        .whitelist_function("rtnl_qdisc_tbf_set_rate")
+        .whitelist_function("nl_object_get_type")
+        .whitelist_function("nl_cache_get_first")
+        .whitelist_function("nl_cache_nitems")
+        .whitelist_var("NETLINK_ROUTE")
+        .whitelist_var("AF_UNSPEC")
+        .whitelist_var("NLM_F_REPLACE")
+        .generate()
+        .expect("unable to generate netlink-route bindings");
     let nl_out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     nl_bindings
         .write_to_file(nl_out_path.join("libnl.rs"))
@@ -46,7 +46,7 @@ fn main() {
     libccp_make.wait().expect("libccp make spawned but failed");
 
     println!("cargo:rustc-link-search=./libccp");
-    println!("cargo:rustc-link-lib=ccp");
+    println!("cargo:rustc-link-lib=static=ccp");
 
     let ccp_bindings = bindgen::Builder::default()
         .header("./libccp/ccp.h")
