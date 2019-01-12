773735f8e4853082b7644cdd835df2e43e7b05bd
Akshay Narayan <akshayn@mit.edu>
2019-01-12 22:18:41 +0000

    qdisc: oops: Hardcoded/overlapping arguments

--- qdisc/setup-bundler-qdisc.sh
+++ qdisc/setup-bundler-qdisc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ETH=10gp1
-TC_DIR=/home/frank/bundler/qdisc/iproute2/tc
+TC_DIR=/home/`whoami`/bundler/qdisc/iproute2/tc
 SPORT=28316
 SIP=10.1.1.2
-LIMIT=$1
+LIMIT=$2
 
 echo "=> remove qdisc"
 sudo env TC_LIB_DIR=$TC_DIR tc qdisc del dev $ETH root
@@ -13,7 +13,7 @@ echo "=> remove module"
 sudo rmmod sch_bundle_inbox
 
 echo "=> make $1"
-make $1 || exit
+make QTYPE=$1 || exit
 
 echo "=> insmod"
 sudo insmod sch_bundle_inbox.ko || exit
@@ -27,8 +27,8 @@ echo "===> filter out-of-band pkts to child 1"
 sudo tc filter add dev $ETH parent 1: protocol ip prio 1 u32 match ip protocol 17 0xff match ip sport $SPORT 0xffff match ip src $SIP flowid 1:1 || exit
 
 echo "===> add qdisc child 2 (bundle_inbox) with queue size $(LIMIT)"
-sudo env TC_LIB_DIR=/home/frank/bundler/qdisc/iproute2/tc tc qdisc add dev 10gp1 parent 1:2 bundle_inbox rate 100mbit burst 1mbit limit $LIMIT || exit
+sudo env TC_LIB_DIR=$TC_DIR tc qdisc add dev 10gp1 parent 1:2 bundle_inbox rate 100mbit burst 1mbit limit $LIMIT || exit
 # echo "===> filter everything else to child 2"
 # sudo tc filter add dev 10gp1 parent 1: protocol ip prio 2 u32 match ip dst 0.0.0.0/0 flowid 1:2
 
-sudo env TC_LIB_DIR=/home/frank/bundler/qdisc/iproute2/tc tc -s qdisc show dev 10gp1
+sudo env TC_LIB_DIR=$TC_DIR tc -s qdisc show dev 10gp1
