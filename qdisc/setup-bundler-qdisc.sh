411c8acbdf1a9711a14145cabda4df86bb3663c4
Akshay Narayan <akshayn@mit.edu>
2019-02-01 18:35:21 +0000

    qdisc: Fix setup script bug

--- qdisc/setup-bundler-qdisc.sh
+++ qdisc/setup-bundler-qdisc.sh
@@ -26,7 +26,7 @@ sudo tc qdisc add dev $ETH parent 1:1 pfifo_fast || exit
 echo "===> filter out-of-band pkts to child 1"
 sudo tc filter add dev $ETH parent 1: protocol ip prio 1 u32 match ip protocol 17 0xff match ip sport $SPORT 0xffff match ip src $SIP flowid 1:1 || exit
 
-echo "===> add qdisc child 2 (bundle_inbox) with queue size $(LIMIT)"
+echo "===> add qdisc child 2 (bundle_inbox) with queue size $LIMIT"
 sudo env TC_LIB_DIR=$TC_DIR tc qdisc add dev 10gp1 parent 1:2 bundle_inbox rate 100mbit burst 1mbit limit $LIMIT || exit
 # echo "===> filter everything else to child 2"
 # sudo tc filter add dev 10gp1 parent 1: protocol ip prio 2 u32 match ip dst 0.0.0.0/0 flowid 1:2
